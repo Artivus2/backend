ebbdb08c42bf05eabc8eeaf43d40d39705d0d04d
Ilya <105770380+Artivus2@users.noreply.github.com>
2024-04-14 14:36:25 +0700

    ok

--- modules/api/controllers/PaymentController.php
+++ modules/api/controllers/PaymentController.php
@@ -29,6 +29,36 @@ use PHPMailer\PHPMailer\Exception;
 class PaymentController extends BaseController
 {
     
+    
+    public function actionCheckBalance() {
+        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $ipn_key = 'xk8OoaVpKYOWI7mPoeXwl9azuBd+dL4A';
+        $api_key = 'THBJKRT-Y5EMJSM-H95YDKQ-1RFRWS8';
+        $tid = '477bf661-8cfb-428a-9ba9-1aba92dece9a';
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://api.nowpayments.io/v1/balance',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+        'x-api-key: '.$api_key
+        ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        $data = json_decode($response, true); 
+        return $data;
+    }
+
+    
     public function actionCheckPayment() {
         Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
         $ipn_key = 'xk8OoaVpKYOWI7mPoeXwl9azuBd+dL4A';
