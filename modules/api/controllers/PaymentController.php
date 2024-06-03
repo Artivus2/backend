d0d250f83c135d1e516e6ef8c35336d391bbf324
Ilya <105770380+Artivus2@users.noreply.github.com>
2024-06-04 06:54:27 +0700

    ok

--- modules/api/controllers/PaymentController.php
+++ modules/api/controllers/PaymentController.php
@@ -328,17 +328,42 @@ class PaymentController extends BaseController
         
         Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
 
-        $client = new Client();
-        $data[] = [
-            "email" => Yii::$app->request->post("email"),
-            "password" => Yii::$app->request->post("password")            
-        ];
-        $response = $client->createRequest()
-        ->setMethod('POST')
-        ->setUrl('http://127.0.0.1:8001/get_jwt_token?email='.Yii::$app->request->post("email").'&password'.Yii::$app->request->post("password"))
-        ->send();
-        $result=$response;
-        return $result->getContent();
+        // $client = new Client();
+        // $data[] = [
+        //     "email" => Yii::$app->request->post("email"),
+        //     "password" => Yii::$app->request->post("password")            
+        // ];
+        // $response = $client->createRequest()
+        // ->setMethod('POST')
+        // ->setUrl('http://127.0.0.1:8001/get_jwt_token?email='.Yii::$app->request->post("email").'&password'.Yii::$app->request->post("password"))
+        // ->send();
+        // $result=$response;
+        // return $result->getContent();
+        $curl = curl_init();
+        // https://api.nowpayments.io/v1/auth
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.nowpayments.io/v1/auth',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS =>'{
+              "email": "artivus2@gmail.com",
+              "password": "Adm142!@" 
+          }',
+            CURLOPT_HTTPHEADER => array(
+              'Content-Type: application/json'
+            ),
+          ));
+          
+          $response = curl_exec($curl);
+          $auth = json_decode($response, true);
+          $token = $auth["token"];
+
+          return $token;
     }
 
 
