b07fba686375a6c94af74861947b78da0b019dff
Ilya <artivus@gmail.com>
2024-06-05 13:41:52 +0700

    ok

--- modules/api/controllers/PaymentController.php
+++ modules/api/controllers/PaymentController.php
@@ -115,7 +115,7 @@ class PaymentController extends BaseController
         
         $data = [
             "amount" => Yii::$app->request->post("price_amount", 100),
-            "currency" => Yii::$app->request->post("price_currency","usttrc20"),
+            "currency" => Yii::$app->request->post("price_currency","usdttrc20"),
             "order_id" => Yii::$app->request->post("order_id","1"),
             "pay_currency" => Yii::$app->request->post("pay_currency","btc")            
         ];
@@ -252,43 +252,44 @@ class PaymentController extends BaseController
      */
     public function actionGetJwtToken() {
         
-        // $data = [
-        //     "email" => Yii::$app->request->post("email","artivus2@gmail.com"),
-        //     "password" => Yii::$app->request->post("password","Adm142!@")            
-        // ];
-        // $client = new Client([
-        //     'baseUrl' => 'http://127.0.0.1:8001/', 
-        //     'requestConfig' => [
-        //         'format' => Client::FORMAT_JSON
-        //         ],
-        //     'responseConfig' => [
-        //     'format' => Client::FORMAT_JSON
-        //     ],
-        // ]);
-        // $response = $client->post('get_jwt_token', $data)->send();
-        $curl = curl_init();
+        $data = [
+            "email" => Yii::$app->request->post("email","artivus2@gmail.com"),
+            "password" => Yii::$app->request->post("password","Adm142!@")            
+        ];
+        $client = new Client([
+            'baseUrl' => 'http://127.0.0.1:8001/', 
+            'requestConfig' => [
+                'format' => Client::FORMAT_JSON
+                ],
+            'responseConfig' => [
+            'format' => Client::FORMAT_JSON
+            ],
+        ]);
+        $response = $client->post('get_jwt_token', $data)->send();
 
-        curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://api.nowpayments.io/v1/auth',
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS =>'{
-            "email": "'.Yii::$app->request->post("email").'",
-            "password": "'.Yii::$app->request->post("password").'"
-        }',
-        CURLOPT_HTTPHEADER => array(
-            'Content-Type: application/json'
-        ),
-        ));
 
-        $response = curl_exec($curl);
+        // $curl = curl_init();
+        // curl_setopt_array($curl, array(
+        // CURLOPT_URL => 'https://api.nowpayments.io/v1/auth',
+        // CURLOPT_RETURNTRANSFER => true,
+        // CURLOPT_ENCODING => '',
+        // CURLOPT_MAXREDIRS => 10,
+        // CURLOPT_TIMEOUT => 0,
+        // CURLOPT_FOLLOWLOCATION => true,
+        // CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        // CURLOPT_CUSTOMREQUEST => 'POST',
+        // CURLOPT_POSTFIELDS =>'{
+        //     "email": "'.Yii::$app->request->post("email").'",
+        //     "password": "'.Yii::$app->request->post("password").'"
+        // }',
+        // CURLOPT_HTTPHEADER => array(
+        //     'Content-Type: application/json'
+        // ),
+        // ));
 
-        curl_close($curl);
+        // $response = curl_exec($curl);
+
+        // curl_close($curl);
         return $response;
     }
 
@@ -339,8 +340,8 @@ class PaymentController extends BaseController
         'responseConfig' => [
             'format' => Client::FORMAT_JSON
         ],]);
-        $payment_id = Yii::$app->request->get("payment_id","4817895924");
-        $response = $client->get('get_payment_status', ['payment_id' => $payment_id])->send();
+        $payment_id = Yii::$app->request->get("payment_id");
+        $response = $client->get('get_payment_status/'.$payment_id)->send();
         return $response;
     }
 
