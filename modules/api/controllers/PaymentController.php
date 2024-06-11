3185e256f9273d3c72562712af1478778ee5fa89
Ilya <artivus@gmail.com>
2024-06-11 14:50:34 +0700

    ok

--- modules/api/controllers/PaymentController.php
+++ modules/api/controllers/PaymentController.php
@@ -338,12 +338,42 @@ class PaymentController extends BaseController
 
     }
 
-   
+    
+    public function actionGetAdminBalance() {
+        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $api_key = '2WMC682-ATF4WCE-NW0HZNC-5E7S427';
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://api.nowpayments.io/v1/balance',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'x-api-key: '.$api_key
+        ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        $data = json_decode($response, true);
+
+        return $data;
+        
+    }
+
+
     public function actionGetPayout() {
         
         Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
         $api_key = '2WMC682-ATF4WCE-NW0HZNC-5E7S427';
         //$payout_id = "5000885884";
+        $curl = curl_init();
         curl_setopt_array($curl, array(
             CURLOPT_URL => 'https://api.nowpayments.io/v1/payout/'.Yii::$app->request->get("payout_id"),
             CURLOPT_RETURNTRANSFER => true,
