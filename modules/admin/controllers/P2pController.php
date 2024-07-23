c725f9d7da9de5ec356521c3175bb7a31a3074fb
Ilya <105770380+Artivus2@users.noreply.github.com>
2024-07-23 23:07:57 +0700

    ok

--- modules/admin/controllers/P2pController.php
+++ modules/admin/controllers/P2pController.php
@@ -144,73 +144,75 @@ class P2pController extends Controller
     {
         $model = $this->findModelHistory($id);
 
-        $p2p_ads = P2pAds::find()->where(['id' => $model->p2p_ads_id, 'status' => [-1,6]])->one();
-        if (!$p2p_ads) {
-            Yii::$app->response->statusCode = 401;
-            return ["success" => false, "message" => "Ордер не найден"];
-        }
-        if ($p2p_ads->type == 2) {
-            $p2p_h = P2pHistory::find()->where(['id' => $id, 'status' => $model->status])->one();
-            if (!$p2p_h) {
-                Yii::$app->response->statusCode = 400;
-                return ["success" => false, "message" => "Сделка не найдена (в истории продаж)"];
+        if ($model->status == 5) {
+            $p2p_ads = P2pAds::find()->where(['id' => $model->p2p_ads_id, 'status' => [-1,6]])->one();
+            if (!$p2p_ads) {
+                Yii::$app->response->statusCode = 401;
+                return ["success" => false, "message" => "Ордер не найден"];
             }
-            //подтвердил оплату админ после выяснения
-            if ($p2p_ads->amount == 0) {
-                $p2p_ads->status = 10;
-            } else {
-                $p2p_ads->status = -1;
-                if ($p2p_ads->min_limit / $p2p_ads->course > $p2p_ads->amount) {
-                    $p2p_ads->min_limit = $p2p_ads->amount * $p2p_ads->course;
+            if ($p2p_ads->type == 2) {
+                $p2p_h = P2pHistory::find()->where(['id' => $id, 'status' => $model->status])->one();
+                if (!$p2p_h) {
+                    Yii::$app->response->statusCode = 400;
+                    return ["success" => false, "message" => "Сделка не найдена (в истории продаж)"];
+                }
+                //подтвердил оплату админ после выяснения
+                if ($p2p_ads->amount == 0) {
+                    $p2p_ads->status = 10;
+                } else {
+                    $p2p_ads->status = -1;
+                    if ($p2p_ads->min_limit / $p2p_ads->course > $p2p_ads->amount) {
+                        $p2p_ads->min_limit = $p2p_ads->amount * $p2p_ads->course;
+                    }
+                }
+                $p2p_h->status = 4;
+                $wallet_seller = Wallet::findOne(["user_id" => $p2p_h->author_id, 'chart_id' => $p2p_ads->chart_id,'type' => 0]);
+                if (!$wallet_seller) {
+                    $wallet_seller = new Wallet(["user_id" => $p2p_h->author_id, "chart_id" => $p2p_ads->chart_id, "type" => 0]);
+                }
+                $wallet_seller->balance += $p2p_h->price;
+                if(!$wallet_seller->save()) {
+                    Yii::$app->response->statusCode = 400;
+                    return ["success" => false, "message" => "Ошибка сохранения кошелька"];
                 }
             }
-            $p2p_h->status = 4;
-            $wallet_seller = Wallet::findOne(["user_id" => $p2p_h->author_id, 'chart_id' => $p2p_ads->chart_id,'type' => 0]);
-            if (!$wallet_seller) {
-                $wallet_seller = new Wallet(["user_id" => $p2p_h->author_id, "chart_id" => $p2p_ads->chart_id, "type" => 0]);
-            }
-            $wallet_seller->balance += $p2p_h->price;
-            if(!$wallet_seller->save()) {
-                Yii::$app->response->statusCode = 400;
-                return ["success" => false, "message" => "Ошибка сохранения кошелька"];
-            }
-        }
 
-        //  typw1
-        if ($p2p_ads->type == 1) {
-            $p2p_h = P2pHistory::find()->where(['id' => $id,  'status' => $model->status])->one();
-            if (!$p2p_h) {
-                Yii::$app->response->statusCode = 400;
-                return ["success" => false, "message" => "Сделка не найдена (в истории покупок)"];
-            }
-            //подтвердил оплату 
-            if($p2p_ads->amount == 0) {
-                $p2p_ads->status = 10;
-            } else {
-                $p2p_ads->status = -1;
-                if ($p2p_ads->min_limit / $p2p_ads->course > $p2p_ads->amount) {
-                    $p2p_ads->min_limit = $p2p_ads->amount * $p2p_ads->course;
+            //  typw1
+            if ($p2p_ads->type == 1) {
+                $p2p_h = P2pHistory::find()->where(['id' => $id,  'status' => $model->status])->one();
+                if (!$p2p_h) {
+                    Yii::$app->response->statusCode = 400;
+                    return ["success" => false, "message" => "Сделка не найдена (в истории покупок)"];
                 }
-            }
-            $p2p_h->status = 4;
-            $wallet_buyer = Wallet::findOne(["user_id" => $p2p_h->creator_id, 'chart_id' => $p2p_ads->chart_id,'type' => 0]);
-            if (!$wallet_buyer) {
-                $wallet_buyer = new Wallet(["user_id" => $p2p_h->creator_id, "chart_id" => $p2p_ads->chart_id,'type' => 0]);
-            }
-            $wallet_buyer->balance += $p2p_h->price;
-            if(!$wallet_buyer->save()) {
-                Yii::$app->response->statusCode = 400;
-                return ["success" => false, "message" => "Ошибка сохранения кошелька"];
-            }
+                //подтвердил оплату 
+                if($p2p_ads->amount == 0) {
+                    $p2p_ads->status = 10;
+                } else {
+                    $p2p_ads->status = -1;
+                    if ($p2p_ads->min_limit / $p2p_ads->course > $p2p_ads->amount) {
+                        $p2p_ads->min_limit = $p2p_ads->amount * $p2p_ads->course;
+                    }
+                }
+                $p2p_h->status = 4;
+                $wallet_buyer = Wallet::findOne(["user_id" => $p2p_h->creator_id, 'chart_id' => $p2p_ads->chart_id,'type' => 0]);
+                if (!$wallet_buyer) {
+                    $wallet_buyer = new Wallet(["user_id" => $p2p_h->creator_id, "chart_id" => $p2p_ads->chart_id,'type' => 0]);
+                }
+                $wallet_buyer->balance += $p2p_h->price;
+                if(!$wallet_buyer->save()) {
+                    Yii::$app->response->statusCode = 400;
+                    return ["success" => false, "message" => "Ошибка сохранения кошелька"];
+                }
 
-        }
-        //history
-        if(!$p2p_ads->save()) {
-            Yii::$app->response->statusCode = 400;
-            return ["success" => false, "message" => "Ошибка сохранения сделки"];
-        }
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->p2p_ads_id]);
+            }
+            //history
+            if(!$p2p_ads->save()) {
+                Yii::$app->response->statusCode = 400;
+                return ["success" => false, "message" => "Ошибка сохранения сделки"];
+            }
+            if ($model->load(Yii::$app->request->post()) && $model->save()) {
+                return $this->redirect(['view', 'id' => $model->p2p_ads_id]);
+            }
         }
 
         return $this->render('updatehistory', [
